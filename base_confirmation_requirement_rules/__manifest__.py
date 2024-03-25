284df45f211ef6caf28e44f4e92f83c1f3d32bc7
manu <manuel.regidor@sygel.es>
2024-03-25 16:45:49 +0100

    T-5698[ADD]base_confirmation_requirement_rules: base module for confirmation requirements

---
+++ base_confirmation_requirement_rules/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base Confirmation Requirement Rules",
+    "version": "15.0.1.0.0",
+    "summary": "Base module to set confirmation requirement rules.",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Base",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/confirmation_requirement_data.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ base_confirmation_requirement_rules/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import confirmation_requirement_mixin
+from . import confirmation_requirement_rule_mixin
+from . import requirement_rule_type_mixin
