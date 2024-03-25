4a711b3d3fd7c92c26738d585c7bcc6e822ca763
manu <manuel.regidor@sygel.es>
2024-03-25 16:45:49 +0100

    T-5698[ADD]base_confirmation_requirement_rules: base module for confirmation requirements

---
+++ base_confirmation_requirement_rules/models/confirmation_requirement_rule_mixin.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ConfirmationRequirementRuleMixin(models.AbstractModel):
+    _name = "confirmation.requirement.rule.mixin"
+    _description = "Confirmation Requirement Rule Mixin"
+
+    name = fields.Char(
+        string="Name",
+        required=True
+    )
+    domain = fields.Char(
+        string="Domain",
+        required=True,
+        default="[]"
+    )
+    error_description = fields.Text(
+        string="Error Description",
+        required=True,
+        translate=True
+    )
